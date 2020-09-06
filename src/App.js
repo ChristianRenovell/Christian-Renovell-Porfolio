2dab434bef8b5e31357c194b394449484a7bb16c
Christian <christianrenovel83@gmail.com>
2020-09-06 12:48:49 +0100

    animación nav

--- src/App.js
+++ src/App.js
@@ -103,32 +103,9 @@ class App extends Component {
           <div className="section">
             <div id="intro-text" className="mb-5">
               <nav className="navbar navbar-expand-lg">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="">
-                    <img src={burger} width="50px" />
-                  </span>
-                </button>
                 <Burger mode={this.state.mode }
                         onClick={this.showItens}/>
-                <div className="collapse navbar-collapse textcolor" id="navbarTogglerDemo01">
-                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                      <Link to={"/home"} id="l1" className="navbar-brand lin">Inicio</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to={`/profile/`} className="navbar-brand lin">sobre mí</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to={`/proyects/`} className="navbar-brand lin">Proyectos</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to={`/Knowledge/`} className="navbar-brand lin">Conocimientos</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to={`/contact/`} className="navbar-brand lin">Contacto</Link>
-                    </li>
-                  </ul>
-                </div>
+                
                 <div id="switch" onClick={this.mode} >
                   <div id="circle" ></div>
                 </div>
--- src/components/burger/Burger.jsx
+++ src/components/burger/Burger.jsx
@@ -1,34 +1,55 @@
 import React from "react";
+import {Link } from 'react-router-dom';
 import "./style.css";
 
 export default function Burger(props) {
   const [menuOpen, setMenuOpen] = React.useState(false);
   let burger;
- 
 
-  if(props.mode==="light"){
+
+  if (props.mode === "light") {
     burger = "burger";
+
+  }
+  if (props.mode === "darck") {
+    burger = "burger2";
+
+  }
+  function handleOpenMenu() {
+
+    setMenuOpen(!menuOpen);
  
   }
-  if(props.mode==="darck"){
-    burger = "burger2";
-   
-  }
-
-
-  function handleOpenMenu() {  
-      
-    setMenuOpen(!menuOpen);
-  }
-
   return (
     <div className="burg" >
       <div
         className={`menu-btn ${menuOpen ? "open" : "closed"}`}
         onClick={handleOpenMenu}
-        
       >
-        <div className={burger}/>
+        <div className={burger} />
+        <div 
+          className={`${menuOpen ? "showMenu" : "navmenu"}`}
+        >
+         <div className=" navbar-collapse textcolor" id="navbarTogglerDemo01">
+                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                      <Link to={"/home"} id="l1" className="navbar-brand lin ">Inicio</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={`/profile/`} className="navbar-brand lin">sobre mí</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={`/proyects/`} className="navbar-brand lin">Proyectos</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={`/Knowledge/`} className="navbar-brand lin">Conocimientos</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={`/contact/`} className="navbar-brand lin">Contacto</Link>
+                    </li>
+                  </ul>
+                </div>
+        </div>
       </div>
     </div>
   );
