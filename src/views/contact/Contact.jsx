ffb16df3edd5d40c548c47a911326874527f2eb4
Christian <christianrenovel83@gmail.com>
2020-09-21 16:03:19 +0100

    pruebas formulario

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = (props) => {
                   <section class="pb-5 mt-4">
                     <div class="row">
                       <div class="col-md-12 mb-md-0 mb-5">
-                        <form id="contact-form" name="contact" method="POST" data-netlify="true">
+                        <form id="contact-form" name="contact" method="POST">
                           <div class="row">
                             <div class="col-md-6">
                               <div class="md-form mb-4">
