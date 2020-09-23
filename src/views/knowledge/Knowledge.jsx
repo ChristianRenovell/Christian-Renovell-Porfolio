e318add08272acfcae32d5ac8addedf4d9c0da72
Christian <christianrenovel83@gmail.com>
2020-09-23 21:38:51 +0100

    prueba diplomas

--- src/views/knowledge/Knowledge.jsx
+++ src/views/knowledge/Knowledge.jsx
@@ -35,7 +35,7 @@ const Knowledge = (props) => {
                                         title="Git"
                                         description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
                                         diplomaTitle = "OpenWebinars"
-                                        diplomaSrc = {`./images/diploma-git-${props.mode}.png`}
+                                        diplomaSrc = {`https://christian-renovell-porfolio.netlify.app/diploma-git-${props.mode}.png`}
                                     />
                                 </div>
                                 <div className="col-6 col-sm-6 col-md-3 mt-4 ico">
