4bc5dab99025a9acced2e169d03b174a4bbdefb4
Christian <christianrenovel83@gmail.com>
2020-09-23 20:57:35 +0100

    skills

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -20,7 +20,7 @@ const Profile = (props) => {
                 <div className="contentComponent">
                     <div className="content pl-5 pr-5">
                         <div className="container profileContent">
-                            <h1 className="title mt-5 mb-3">¿Quién soy?</h1>
+                            <h1 className="title mt-3 mb-3">¿Quién soy?</h1>
                             <div className="lineTitle" style={{ backgroundColor: color }} />
                         </div>
                         <div className="p1 mt-3">
@@ -40,47 +40,48 @@ const Profile = (props) => {
                         </div>
                         <div className="container mt-3 content--inner4">
                             <div className="skills container">
-                                <div className="section-title mt-5">
-                                    <h2>Skills</h2>
+                                <div className="section-title mt-5 mb-3">
+                                    <h1>Skills</h1>
+                                    <div className="lineTitle mb-4" style={{ backgroundColor: color }} />
                                 </div>
                                 <div className="row skills-content">
                                     <div className="col-lg-6">
                                         <div className="progress">
                                             <span className="skill">HTML <i className="val">100%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar html" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">CSS <i className="val">90%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar css" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">JavaScript <i class="val">75%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar javascript" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="col-lg-6">
                                         <div className="progress">
-                                            <span className="skill">PHP <i className="val">80%</i></span>
+                                            <span className="skill">PHP <i className="val">65%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar php" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                         <div className="progress">
-                                            <span className="skill">WordPress/CMS <i class="val">90%</i></span>
+                                            <span className="skill">React.js <i class="val">75%</i></span>
                                             <div class="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar react" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                         <div className="progress">
-                                            <span className="skill">Photoshop <i class="val">55%</i></span>
+                                            <span className="skill">Node.js <i class="val">65%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar node" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                     </div>
