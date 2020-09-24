058a10f68ed610564d42b1c1f4d7d9859aac13b0
Christian <christianrenovel83@gmail.com>
2020-09-24 17:53:15 +0100

    skills color barras

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -49,19 +49,19 @@ const Profile = (props) => {
                                         <div className="progress">
                                             <span className="skill">HTML <i className="val">100%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar html" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar html" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">CSS <i className="val">90%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar css" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar css" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">JavaScript <i class="val">75%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar javascript" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar javascript" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                     </div>
@@ -69,19 +69,19 @@ const Profile = (props) => {
                                         <div className="progress">
                                             <span className="skill">PHP <i className="val">65%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar php" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar php" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">React.js <i class="val">75%</i></span>
                                             <div class="progress-bar-wrap">
-                                                <div className="progress-bar react" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar react" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                         <div className="progress">
                                             <span className="skill">Node.js <i class="val">65%</i></span>
                                             <div className="progress-bar-wrap">
-                                                <div className="progress-bar node" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div className="progress-bar node" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style={{ backgroundColor: color }}></div>
                                             </div>
                                         </div>
                                     </div>
